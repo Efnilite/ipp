848e104f9ea203b6373a8fdcd1fdf0c65cf4f425
Efnilite <35348263+Efnilite@users.noreply.github.com>
2023-04-29 15:33:47 +0200

    Added japanese

--- src/main/java/dev/efnilite/ipp/config/PlusLocales.java
+++ src/main/java/dev/efnilite/ipp/config/PlusLocales.java
@@ -61,6 +61,7 @@ public class PlusLocales {
                         plugin.saveResource("locales/en.yml", false);
                         plugin.saveResource("locales/nl.yml", false);
                         plugin.saveResource("locales/fr.yml", false);
+                        plugin.saveResource("locales/jp.yml", false);
                     }
 
                     // get all files in locales folder
